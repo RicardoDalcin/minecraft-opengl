80992f07cfdd21157832e47582a93ff074c2cc58
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2022-07-29 23:03:14 -0300

    📦 Add support to GLM

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <glad/glad.h>  // Criação de contexto OpenGL 3.3
 #include <GLFW/glfw3.h> // Criação de janelas do sistema operacional
 
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // Headers locais, definidos na pasta "include/"
 #include "utils.h"
 
